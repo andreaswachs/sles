7bb03826283026cfb559e49b5f290eafc87e7b4a
Andreas Wachs <andreas@MacBook-Pro-tilhrende-Andreas.local>
2023-09-06 15:19:21 +0200

    Make work with augmented matrices

--- main.go
+++ main.go
@@ -8,74 +8,38 @@ import (
 	"strings"
 )
 
+var augmentedLinePos = -1
+
 func main() {
 	// initialize an empty coefficients matrix
 	coefficients := [][]float64{}
+	inputLog := []string{}
 
+	// Take input for a new row of coefficients
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter the number of equations: ")
-	equations, err := reader.ReadString('\n')
+
+	fmt.Print("Augmented line position (empty input for no augmented line): ")
+	input, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	equations = strings.TrimSuffix(equations, "\n")
 
-	fmt.Print("Enter the number of variables: ")
-	variables, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	variables = strings.TrimSuffix(variables, "\n")
+	input = strings.TrimSuffix(input, "\n")
 
-	// convert the strings to integers
-	equationsInt, err := strconv.Atoi(equations)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	variablesInt, err := strconv.Atoi(variables)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	variablesInt += 1 // add one for the augmented part
-
-	// initialize the coefficients matrix
-	for i := 0; i < equationsInt; i++ {
-		coefficients = append(coefficients, make([]float64, variablesInt))
-	}
-
-	// read in one row of coefficients at a time
-	for i := 0; i < equationsInt; i++ {
-		fmt.Printf("Enter the coefficients for equation %d: ", i+1)
-		coefficientsString, err := reader.ReadString('\n')
+	if input != "" {
+		augmentedLinePos, err = strconv.Atoi(input)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		coefficientsString = strings.TrimSuffix(coefficientsString, "\n")
-
-		// split the string into a slice of strings
-		coefficientsSlice := strings.Split(coefficientsString, " ")
-
-		// convert the strings to floats
-		for j := 0; j < variablesInt; j++ {
-			coefficients[i][j], err = strconv.ParseFloat(coefficientsSlice[j], 64)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
 	}
 
-	// Now we have the coefficients matrix, so we can do row operations
+	fmt.Println("Input coefficient matrix row by row, separated by spaces when done, input empty line")
 
+	// Take input for a new row of coefficients. If input is empty, stop taking input
 	for {
-		print(coefficients)
 
-		fmt.Print("Enter a row operation: ")
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
@@ -83,6 +47,41 @@ func main() {
 		}
 
 		input = strings.TrimSuffix(input, "\n")
+
+		if input == "" {
+			break
+		}
+
+		// Split the input by spaces
+		coefficientsStr := strings.Split(input, " ")
+
+		// Convert the string coefficients to float64
+		coefficientsFloat := []float64{}
+		for _, coefficientStr := range coefficientsStr {
+			coefficient, err := strconv.ParseFloat(coefficientStr, 64)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			coefficientsFloat = append(coefficientsFloat, coefficient)
+		}
+
+		coefficients = append(coefficients, coefficientsFloat)
+	}
+
+	for {
+		print(coefficients)
+
+		fmt.Print("Row operation: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		input = strings.TrimSuffix(input, "\n")
+		inputLog = append(inputLog, input)
 
 		var row1, row2 int
 		var scalar float64
@@ -95,16 +94,18 @@ func main() {
 			// parse the rows and scalar
 			fmt.Sscanf(input, "add %d %d %f", &row1, &row2, &scalar)
 			coefficients = add(coefficients, row1-1, row2-1, scalar)
-		} else if strings.HasPrefix(input, "sub") {
-			// parse the rows and scalar
-			fmt.Sscanf(input, "sub %d %d %f", &row1, &row2, &scalar)
-			coefficients = add(coefficients, row1-1, row2-1, -scalar)
 		} else if strings.HasPrefix(input, "swap") {
 			// parse the rows
 			fmt.Sscanf(input, "swap %d %d", &row1, &row2)
 			coefficients = swap(coefficients, row1-1, row2-1)
 		} else if strings.HasPrefix(input, "quit") {
 			os.Exit(0)
+		} else if strings.HasPrefix(input, "print") {
+			print(coefficients)
+		} else if strings.HasPrefix(input, "div") {
+			// parse the row and scalar
+			fmt.Sscanf(input, "div %d %f", &row1, &scalar)
+			coefficients = mul(coefficients, row1-1, 1/scalar)
 		} else {
 			fmt.Println("Invalid input")
 		}
@@ -118,11 +119,13 @@ func print(coefficients [][]float64) {
 	for i := 0; i < len(coefficients); i++ {
 		fmt.Print("[ ")
 		for j := 0; j < len(coefficients[i]); j++ {
-			// Print the augmented part with a nice format
-			if j == len(coefficients[i])-1 {
+			// Check if need to print augmented line
+			if augmentedLinePos == j {
 				fmt.Print("| ")
 			}
-			fmt.Printf("%f ", coefficients[i][j])
+
+			// Print coefficient with 2 decimal places
+			fmt.Printf("%.2f\t", coefficients[i][j])
 		}
 		fmt.Println("]")
 	}
